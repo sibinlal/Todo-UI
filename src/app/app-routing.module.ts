f06634f6cf66fae0e63860930a8cacfd0afe7a09
sibinlal <sbnlalpnr@gmail.com>
2023-12-13 23:06:25 +0530

    added routes config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './components/others/notfound/notfound.component';
+import { TodolistComponent } from './components/todo/todolist/todolist.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { SignupComponent } from './components/user/signup/signup.component';
+import { NetworkErrorComponent } from './components/others/network-error/network-error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/login', pathMatch:'full'},
+  {path:'**', component:NotfoundComponent},
+  {path:'todo', component:TodolistComponent},
+  {path:'login', component:LoginComponent},
+  {path:'signUp', component:SignupComponent},
+  {path:'network-error', component:NetworkErrorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
