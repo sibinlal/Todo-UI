c9617b05347fd279c3c38f6d8e25168be508bdf2
sibinlal <sbnlalpnr@gmail.com>
2023-12-29 19:10:17 +0530

    login page

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  username!: string;
+  password!: string;
+
+  loginForm: FormGroup;
+
+  constructor(private api:UserService, private toaster: ToastrService, private router:Router) {
+    this.loginForm = new FormGroup({
+      email:new FormControl('',[Validators.required, Validators.email]),
+      password: new FormControl('',[Validators.required, Validators.minLength(8)])
+    });
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onSubmit() {
+
+    if(this.loginForm.valid){
+        
+    }
+
+  }
 
 }
